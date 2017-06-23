e6e53dc07a56ed2d6b5003f6ee609579c0efa72c
mdadil2019 <mdadil2019@gmail.com>
2017-06-23 06:46:54 +0530

    .

--- app/src/main/java/com/environer/becofriend/adapter/ContentAdapter.java
+++ app/src/main/java/com/environer/becofriend/adapter/ContentAdapter.java
@@ -1,6 +1,7 @@
 package com.environer.becofriend.adapter;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
